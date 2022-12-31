0a646e391af4ead92de67aed6c8083dc45eeae61
RuEntrySet <t9154716172@gmail.com>
2022-12-31 14:15:15 +0300

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
  
     shadow(files(library + "vault.jar"))
 
-    implementation("ru.entryset:api:3.2.4")
+    implementation("ru.entryset:api:3.2.5")
     implementation("redis.clients:jedis:4.2.0")
 
     compileOnly("me.clip:placeholderapi:2.10.9")
