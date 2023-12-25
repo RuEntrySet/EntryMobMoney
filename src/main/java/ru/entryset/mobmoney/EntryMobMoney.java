ee0c9ec47a4dffa66db9065bd18b7fd6b6013ea1
RuEntrySet <t9154716172@gmail.com>
2023-12-25 20:13:14 +0300

    commit

--- src/main/java/ru/entryset/mobmoney/EntryMobMoney.java
+++ src/main/java/ru/entryset/mobmoney/EntryMobMoney.java
@@ -4,6 +4,7 @@ import ru.entryset.core.EntryCore;
 import ru.entryset.core.bukkit.bukkit.configuration.Config;
 import ru.entryset.core.bukkit.bukkit.manager.Messager;
 import ru.entryset.core.expansion.Expansion;
+import ru.entryset.mobmoney.events.Events;
 
 public class EntryMobMoney extends Expansion {
 
